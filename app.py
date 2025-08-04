7d374614ae9a9a73556acc598bff2aca07f66957
Smartfab <smartfabunmi@gmail.com>
2025-08-04 12:23:44 -0400

    add total balance feature

--- app.py
+++ app.py
@@ -94,8 +94,10 @@ def search_transactions():
 # Total Balance
 @app.route("/balance")
 def total_balance():
-    
-
+    for transaction in transactions:
+        total_balance = float(sum(transaction.amount))
+    return redirect_url(url_for("get_transactions"))
+    return  render_template("transactions.html", transactions=transactions)
 
 # Run the Flask app
 if __name__ == "__main__":
